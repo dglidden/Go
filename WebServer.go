5f31697e384439cf83abc84e5667b23f94bb6959
Derek Glidden <dglidden@gmail.com>
2022-01-04 18:34:58 -0500

    refactor funcs, add update password

--- WebServer.go
+++ WebServer.go
@@ -12,6 +12,38 @@ import (
 	"github.com/gorilla/mux"
 )
 
+func CreateUser(username string, password string, db *sql.DB) string {
+	msg := "Creating user... "
+	Query := "INSERT INTO users (username, password, created_at) values (?, ?, ?)"
+	result, err := db.Exec(Query, username, password, time.Now())
+
+	if err != nil {
+		msg += fmt.Sprintf("There was an error creating your user: %s", err.Error())
+	} else {
+		UserID, err := result.LastInsertId()
+		if err != nil {
+			msg += fmt.Sprintf("There was an error retrieving your User ID: %s", err.Error())
+		} else {
+			msg += fmt.Sprintf("Your user was created with ID %d", UserID)
+		}
+	}
+
+	return msg
+}
+
+func UpdatePassword(username string, password string, db *sql.DB) string {
+	msg := "Updating password... "
+	Query := "UPDATE users SET password = ? WHERE username = ?"
+	_, err := db.Exec(Query, password, username)
+	if err != nil {
+		msg += fmt.Sprintf("There was an error updating your password: %s", err.Error())
+	} else {
+		msg += "Updated password"
+	}
+
+	return msg
+}
+
 func main() {
 	r := mux.NewRouter()
 
@@ -29,18 +61,17 @@ func main() {
 		username := vars["username"]
 		password := vars["password"]
 
-		Query := `INSERT INTO users (username, password, created_at) values (?, ?, ?)`
+		Query := "SELECT * FROM users WHERE username = ?"
 
-		result, err := db.Exec(Query, username, password, time.Now())
-		if err != nil {
-			fmt.Fprintf(w, "There was an error creating your user: %s", err.Error())
-		} else {
-			UserID, err := result.LastInsertId()
-			if err != nil {
-				fmt.Fprintf(w, "There was an error retrieving your User ID: %s", err.Error())
+		result, err := db.Query(Query, username)
+		if err == nil {
+			if result.Next() {
+				fmt.Fprintf(w, UpdatePassword(username, password, db))
 			} else {
-				fmt.Fprintf(w, "Your user was created with ID %d", UserID)
+				fmt.Fprintf(w, CreateUser(username, password, db))
 			}
+		} else {
+			fmt.Fprintf(w, "There was an error looking up your username %s", err.Error())
 		}
 	})
 
