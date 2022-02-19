fb05b61ec9df7e91a42a5371aeb0507e4d35daac
Derek Glidden <dglidden@gmail.com>
2022-02-18 22:57:55 -0500

    Added a lookup type function

--- WebServer.go
+++ WebServer.go
@@ -14,7 +14,7 @@ import (
 
 func CreateUser(username string, password string, db *sql.DB) string {
 	msg := "Creating user... "
-	Query := "INSERT INTO users (username, password, created_at) values (?, ?, ?)"
+	Query := "INSERT INTO USERS (username, password, created_at) values (?, ?, ?)"
 	result, err := db.Exec(Query, username, password, time.Now())
 
 	if err != nil {
@@ -33,7 +33,7 @@ func CreateUser(username string, password string, db *sql.DB) string {
 
 func UpdatePassword(username string, password string, db *sql.DB) string {
 	msg := "Updating password... "
-	Query := "UPDATE users SET password = ? WHERE username = ?"
+	Query := "UPDATE USERS SET PASSWORD = ? WHERE USERNAME = ?"
 	_, err := db.Exec(Query, password, username)
 	if err != nil {
 		msg += fmt.Sprintf("There was an error updating your password: %s", err.Error())
@@ -56,12 +56,39 @@ func main() {
 		log.Fatal(err)
 	}
 
+	r.HandleFunc("/db/{username}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		username := vars["username"]
+
+		Query := "SELECT * FROM USERS WHERE username = ?"
+
+		result, err := db.Query(Query, username)
+		if err == nil {
+			defer result.Close()
+			if result.Next() {
+				var id int64
+				var password string
+				var createdAt time.Time
+				err := result.Scan(&id, &username, &password, &createdAt)
+				if err != nil {
+					fmt.Fprintf(w, "There was an error looking up your username %s", err.Error())
+				} else {
+					fmt.Fprintf(w, "The result of your query is: %d, %s, %s, %s", id, username, password, createdAt)
+				}
+			} else {
+				fmt.Fprintf(w, "We were not able to find that username in the database")
+			}
+		} else {
+			fmt.Fprintf(w, "There was an error looking up your username %s", err.Error())
+		}
+	})
+
 	r.HandleFunc("/db/{username}/{password}", func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
 		username := vars["username"]
 		password := vars["password"]
 
-		Query := "SELECT * FROM users WHERE username = ?"
+		Query := "SELECT * FROM USERS WHERE username = ?"
 
 		result, err := db.Query(Query, username)
 		if err == nil {
